a5378a54e8828cd2b3e2df3c4353419939c9b6dc
reeFridge <reefridgerator@gmail.com>
2017-10-09 22:14:18 +0700

    Basic scene switching logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate piston_window;
 extern crate byteorder;
+
 mod scenes;
 mod connection;
 
 use piston_window::*;
-use scenes::play;
+use scenes::{play, menu};
+use scenes::scene::Scene;
 
 pub fn main() {
     let mut window: PistonWindow = WindowSettings::new("side-run", [800, 600])
@@ -13,29 +15,44 @@ pub fn main() {
         .build()
         .unwrap();
 
-    let mut state = play::State::new().unwrap();
-
-    match state.connect("127.0.0.1:7001".to_string()) {
-        Ok(_) => (),
-        Err(e) => println!("Failed to connect: {}", e)
-    };
+    let mut scene_idx = 0 as usize;
+    let mut scenes: Vec<Box<Scene>> = vec![
+        Box::new(play::Play::new(Some("127.0.0.1:7001".to_string()))),
+        Box::new(menu::Menu::new())
+    ];
 
     while let Some(e) = window.next() {
         match e {
             Event::Input(Input::Button(ButtonArgs { state: button_state, button, .. })) => {
                 match button_state {
-                    ButtonState::Press => state.key_press(button),
+                    ButtonState::Press => {
+                        match button {
+                            Button::Keyboard(Key::Return) => {
+                                scene_idx = match scene_idx {
+                                    0 => 1,
+                                    1 => 0,
+                                    _ => 0
+                                };
+                            }
+                            _ => ()
+                        }
+                    }
                     _ => ()
                 }
-            },
+
+                match button_state {
+                    ButtonState::Press => scenes[scene_idx].key_press(button),
+                    _ => ()
+                }
+            }
             Event::Loop(Loop::Render(args)) => {
                 window.draw_2d(&e, |mut ctx, mut graph| {
-                    state.draw(&mut ctx,&mut graph).unwrap();
+                    scenes[scene_idx].draw(&mut ctx, &mut graph).unwrap();
                 });
-            },
+            }
             Event::Loop(Loop::Update(UpdateArgs { dt })) => {
-                state.update(dt).unwrap();
-            },
+                scenes[scene_idx].update(dt).unwrap();
+            }
             _ => ()
         };
     }
