c92c3ad8a8f201c7714553c9e67049646ed336c0
reefridge <reefridgerator@gmail.com>
2017-10-12 01:03:56 +0700

    Updated main

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,11 @@ extern crate byteorder;
 
 mod scenes;
 mod connection;
+mod game_cycle;
 
 use piston_window::*;
-use scenes::{play, menu};
-use scenes::scene::Scene;
-
-struct GameCycle {
-    scene: Box<Scene>
-}
-
-impl GameCycle {
-    fn new(scene: Box<Scene>) -> GameCycle {
-        GameCycle { scene: scene }
-    }
-
-    fn open_scene(&mut self, scene: Box<Scene>) {
-        self.scene = scene;
-    }
-
-    fn run(&mut self, window: &mut PistonWindow) {
-        loop {
-            let may_event = window.next();
-
-            if let Some(event) = may_event {
-                event.button(|ButtonArgs { state: button_state, button, .. }| {
-                    match button_state {
-                        ButtonState::Press => self.scene.key_press(button),
-                        _ => ()
-                    }
-                });
-
-                event.render(|_| {
-                    window.draw_2d(&event, |mut ctx, mut graph| {
-                        self.scene.draw(&mut ctx, &mut graph).unwrap();
-                    });
-                });
-
-                event.update(|&UpdateArgs { dt }| {
-                    self.scene.update(dt).unwrap();
-                });
-            } else {
-                break;
-            }
-        }
-    }
-}
+use scenes::play::Play;
+use game_cycle::GameCycle;
 
 pub fn main() {
     let mut window: PistonWindow = WindowSettings::new("side-run", [800, 600])
@@ -56,7 +16,7 @@ pub fn main() {
         .build()
         .unwrap();
 
-    let scene = Box::new(play::Play::new(Some("127.0.0.1:7001".to_string())));
+    let scene = Box::new(Play::new(Some("127.0.0.1:7001".to_string())));
     let mut cycle = GameCycle::new(scene);
 
     cycle.run(&mut window);
