78deb76d4ecd18cec94fc80c8a96e06c772010bc
reefridge <reefridgerator@gmail.com>
2017-10-12 00:48:53 +0700

    Introduce switching scene inside scenes

--- src/scenes/scene.rs
+++ src/scenes/scene.rs
@@ -1,8 +1,11 @@
 use piston_window::*;
 use scenes::common::*;
 
+pub type SceneInstance = Box<Scene>;
+
 pub trait Scene {
     fn update(&mut self, dt: f64) -> GameResult<()>;
     fn draw(&mut self, ctx: &mut Context, graphics: &mut G2d) -> GameResult<()>;
     fn key_press(&mut self, button: Button);
+    fn get_next(&mut self) -> Option<SceneInstance>;
 }
