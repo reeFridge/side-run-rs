41384ea91dbaafb8f0cae69b3d89e37acc0edb08
reefridge <reefridgerator@gmail.com>
2017-10-12 00:47:57 +0700

    Moved game cycle implementation to separate file

---
+++ src/game_cycle.rs
@@ -0,0 +1,47 @@
+use piston_window::*;
+use scenes::scene::SceneInstance;
+
+pub struct GameCycle {
+    scene: SceneInstance
+}
+
+impl GameCycle {
+    pub fn new(scene: SceneInstance) -> GameCycle {
+        GameCycle { scene: scene }
+    }
+
+    pub fn run(&mut self, window: &mut PistonWindow) {
+        loop {
+            let may_event = window.next();
+
+            if let Some(event) = may_event {
+                event.button(|ButtonArgs { state: button_state, button, .. }| {
+                    match button_state {
+                        ButtonState::Press => self.scene.key_press(button),
+                        _ => ()
+                    }
+                });
+
+                event.render(|_| {
+                    window.draw_2d(&event, |mut ctx, mut graph| {
+                        self.scene.draw(&mut ctx, &mut graph).unwrap();
+                    });
+                });
+
+                event.update(|&UpdateArgs { dt }| {
+                    self.scene.update(dt).unwrap();
+                });
+
+                self.scene
+                    .get_next()
+                    .and_then(|next_scene| Some(self.set_scene(next_scene)));
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn set_scene(&mut self, scene: SceneInstance) {
+        self.scene = scene;
+    }
+}
