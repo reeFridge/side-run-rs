0ca281fefd758e63c3275e3ea972b882555490e3
reefridge <reefridgerator@gmail.com>
2017-10-14 13:58:03 +0700

    Connect conrod to menu scene

--- src/scenes/menu.rs
+++ src/scenes/menu.rs
@@ -2,26 +2,31 @@ use scenes::common::*;
 use piston_window::*;
 use scenes::scene::{Scene, SceneInstance, BaseSwitcher, Switcher};
 use scenes::play::Play;
+use conrod;
 
 pub struct Menu {
-    switcher: BaseSwitcher
+    switcher: BaseSwitcher,
+    ui: conrod::Ui
 }
 
 impl Menu {
     pub fn new() -> Menu {
         Menu {
-            switcher: BaseSwitcher::new(None)
+            switcher: BaseSwitcher::new(None),
+            ui: conrod::UiBuilder::new([800., 600.]).build()
         }
     }
 }
 
 impl Scene for Menu {
-    fn switcher(&mut self) -> &mut Switcher {
-        &mut self.switcher
+    fn handle_event(&mut self, event: Event) {
+        if let Some(e) = conrod::backend::piston::event::convert(event, 800. as conrod::Scalar,  600. as conrod::Scalar) {
+            self.ui.handle_event(e);
+        }
     }
 
-    fn update(&mut self, dt: f64) -> GameResult<()> {
-        Ok(())
+    fn switcher(&mut self) -> &mut Switcher {
+        &mut self.switcher
     }
 
     fn draw(&mut self, ctx: &mut Context, graphics: &mut G2d) -> GameResult<()> {
