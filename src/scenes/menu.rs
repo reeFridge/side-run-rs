03ef9458ede8580497dff23e6c773f81700fa792
reefridge <reefridgerator@gmail.com>
2017-10-14 19:18:03 +0700

    Using conrod for drawing text using Text widget

--- src/scenes/menu.rs
+++ src/scenes/menu.rs
@@ -1,28 +1,67 @@
 use scenes::common::*;
-use piston_window::*;
 use scenes::scene::{Scene, SceneInstance, BaseSwitcher, Switcher};
 use scenes::play::Play;
-use conrod;
+use find_folder;
+use conrod::{self, widget, Colorable, Positionable, Widget};
+use piston_window::*;
+
+widget_ids!(struct Ids { text });
 
 pub struct Menu {
     switcher: BaseSwitcher,
-    ui: conrod::Ui
+    ui: conrod::Ui,
+    ids: Ids,
+    image_map: conrod::image::Map<G2dTexture>,
+    glyph_cache: conrod::text::GlyphCache,
+    text_texture_cache: G2dTexture
 }
 
 impl Menu {
-    pub fn new() -> Menu {
+    pub fn new(text_texture_cache: G2dTexture) -> Menu {
+        let mut ui = conrod::UiBuilder::new([800., 600.]).build();
+
+        let assets = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
+        let font_path = assets.join("fonts/Terminus.ttf");
+        ui.fonts.insert_from_file(font_path).unwrap();
+
+        const WIDTH: u32 = 800;
+        const HEIGHT: u32 = 600;
+        const SCALE_TOLERANCE: f32 = 0.1;
+        const POSITION_TOLERANCE: f32 = 0.1;
+
         Menu {
             switcher: BaseSwitcher::new(None),
-            ui: conrod::UiBuilder::new([800., 600.]).build()
+            ids: Ids::new(ui.widget_id_generator()),
+            ui: ui,
+            image_map: conrod::image::Map::<G2dTexture>::new(),
+            glyph_cache: conrod::text::GlyphCache::new(WIDTH, HEIGHT, SCALE_TOLERANCE, POSITION_TOLERANCE),
+            text_texture_cache: text_texture_cache
         }
     }
 }
 
 impl Scene for Menu {
     fn handle_event(&mut self, event: Event) {
-        if let Some(e) = conrod::backend::piston::event::convert(event, 800. as conrod::Scalar,  600. as conrod::Scalar) {
+        let size = (800. as conrod::Scalar,  600. as conrod::Scalar);
+
+        if let Some(e) = conrod::backend::piston::event::convert(event, size.0, size.1) {
             self.ui.handle_event(e);
         }
+    }
+
+    fn update(&mut self, _dt: f64) -> GameResult<()> {
+        // Set the widgets.
+        let ui = &mut self.ui.set_widgets();
+
+        // "Hello World!" in the middle of the screen.
+        widget::Text::new("SIDE_RUN\nPress 'enter' to start")
+            .center_justify()
+            .middle_of(ui.window)
+            .color(conrod::color::WHITE)
+            .font_size(32)
+            .set(self.ids.text, ui);
+
+        Ok(())
     }
 
     fn switcher(&mut self) -> &mut Switcher {
@@ -30,7 +69,41 @@ impl Scene for Menu {
     }
 
     fn draw(&mut self, ctx: &mut Context, graphics: &mut G2d) -> GameResult<()> {
-        clear(WHITE, graphics);
+        let mut text_vertex_data = Vec::new();
+
+        // A function used for caching glyphs to the texture cache.
+        let cache_queued_glyphs = |graphics: &mut G2d,
+                                   cache: &mut G2dTexture,
+                                   rect: conrod::text::rt::Rect<u32>,
+                                   data: &[u8]|
+            {
+                let offset = [rect.min.x, rect.min.y];
+                let size = [rect.width(), rect.height()];
+                let format = texture::Format::Rgba8;
+                let encoder = &mut graphics.encoder;
+                text_vertex_data.clear();
+                text_vertex_data.extend(data.iter().flat_map(|&b| vec![255, 255, 255, b]));
+                texture::UpdateTexture::update(cache, encoder, format, &text_vertex_data[..], offset, size)
+                    .expect("failed to update texture")
+            };
+
+        if let Some(primitives) = self.ui.draw_if_changed() {
+            // Specify how to get the drawable texture from the image. In this case, the image
+            // *is* the texture.
+            fn texture_from_image<T>(img: &T) -> &T { img }
+
+            // Draw the conrod `render::Primitives`.
+            conrod::backend::piston::draw::primitives(
+                primitives,
+                ctx.clone(),
+                graphics,
+                &mut self.text_texture_cache,
+                &mut self.glyph_cache,
+                &self.image_map,
+                cache_queued_glyphs,
+                texture_from_image
+            );
+        }
 
         Ok(())
     }
